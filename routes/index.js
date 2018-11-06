437b0695bbaf7ccfbb5687a172ec6e703aa7ab6b
wildhamster26 <38760849+wildhamster26@users.noreply.github.com>
2018-11-06 17:49:08 +0100

    fixed it

--- routes/index.js
+++ routes/index.js
@@ -9,49 +9,48 @@ router.get("/ironhackers/maxence", (req, res, next) => {
   res.render("ironhackers/maxence");
 });
 
-router.get("/ironhackers/matthias", (req, res, next) => {
-  res.render("matthias");
+router.get("/ironhackers/feli", (req, res, next) => {
+  res.render("ironhackers/feli");
 });
-
-router.get("/ironhackers/gutu", (req, res, next) => {
-  res.render("ironhackers/gutu");
-});
-
-router.get("/ironhackers/nele", (req, res, next) => {
-  res.render("ironhackers/nele");
-});
-
 
 router.get("/ironhackers/anna", (req, res, next) => {
   res.render("ironhackers/anna");
-});
-
-router.get("/ironhackers/carlos", (req, res, next) => {
-  res.render("ironhackers/carlos");
 });
 
 router.get("/ironhackers/ziggy", (req, res, next) => {
   res.render("ironhackers/ziggy");
 });
 
-router.get('/ironhackers/samanta', (req, res, next) => {
-  res.render('ironhackers/samanta');
-});
-
-router.get("/ironhackers/feli", (req, res, next) => {
-  res.render("ironhackers/feli");
-});
-
-router.get("/ironhackers/khalil", (req, res, next) => {
-  res.render("ironhackers/khalil");
+router.get("/ironhackers/carlos", (req, res, next) => {
+  res.render("ironhackers/carlos");
 });
 
 router.get("/ironhackers/axel", (req, res, next) => {
   res.render("ironhackers/axel");
 });
 
+router.get("/ironhackers/gutu", (req, res, next) => {
+  res.render("ironhackers/gutu");
+});
+
+router.get('/ironhackers/samanta', (req, res, next) => {
+  res.render('ironhackers/samanta');
+});
+
+router.get("/ironhackers/matthias", (req, res, next) => {
+  res.render("ironhackers/matthias");
+});
+
+router.get("/ironhackers/khalil", (req, res, next) => {
+  res.render("ironhackers/khalil");
+});
+
 router.get("/ironhackers/eullin", (req, res, next) => {
   res.render("ironhackers/eullin");
+});
+
+router.get("/ironhackers/nele", (req, res, next) => {
+  res.render("ironhackers/nele");
 });
 
 module.exports = router;
